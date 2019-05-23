287e743eed49becae42846f5465d673ea23ac3cd
Iliyan Stoyanov <i.stoyanov@fio.de>
2019-05-23 11:56:06 +0300

    Initial commit

---
+++ webapp/src/components/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Login from './../login/Login';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+        <Login />
+    </div>
+  );
+}
+
+export default App;
